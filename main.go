3a2411ec2643e10d1ead9cb6f159198a317fbff7
Amar3339 <amarg3339@gmail.com>
2026-09-24 10:02:41 +0530

    first GET api in go lang

--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
-import("github.com/gin-gonic/gin",
-"fmt",
-"net/http"
+
+import (
+	"net/http"
+
+	"example.com/models"
+	"github.com/gin-gonic/gin"
 )
 func main(){
 	server:=gin.Default()
-	server.GET("events",getEvents)
+	server.GET("/events",getEvents)
+	server.GET("/home",gethome)
+	server.POST("/create",create)
+	server.POST("/events",createEvents)
 	server.Run(":8080") //localhost 8080
 }
-getEvents(context *gin.Contenxt){
-	context.JSON(http.StatusOK,gin.H{"message:My first api response""jai shree mahakal "})
+ func getEvents(context *gin.Context){
+	context.JSON(http.StatusOK,gin.H{"message":"My first api response jai shree mahakal"})
+}
+func gethome(context *gin.Context){
+	context.JSON(http.StatusOK,gin.H{"message":"this is my home page"})
+}
+func createEvents(context *gin.Context){
+	var event models.Event
+	err:=context.ShouldBindJSON(&event)
+	if err !=nil{
+		context.JSON(http.StatusBadRequest,gin.H{"message":"bad response"})
+		return
+	}
+	event.ID=1
+	event.UserID=1
+	event.Save()
+	context.JSON(http.StatusCreated,gin.H{"message":"event created","event":event})
+
+}
+func create(context *gin.Context){
+	var event models.Event
+	err:=context.ShouldBindJSON(&event)
+	if err !=nil{
+context.JSON(http.StatusCreated,gin.H{"message":"api not found ankit"})
+return
+	}
+	event.ID=2
+	event.UserID=2
+	event.Save()
+	context.JSON(http.StatusCreated,gin.H{"mesaage":"anit event is creeate ed","event":event})
 }
